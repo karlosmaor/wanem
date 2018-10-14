86b23d7fdc6296febf74c7e4834836b0a800c73e
karlosmaor <35969364+karlosmaor@users.noreply.github.com>
2018-10-14 11:18:21 -0500

    asd

--- server/main.js
+++ server/main.js
@@ -5,7 +5,7 @@ const app  = require('./app')
 const config = require('../config')
 //const websocket = require('../websocket/server/main')
 
-mongoose.connect(config.db, (err,res)=>{
+mongoose.connect(config.db, { useNewUrlParser: true }, (err,res)=>{
   if(err){
     return console.log(`Error al conectarse a la base de datos: ${err}`);
   }
