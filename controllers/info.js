3e67fb9ad5f5b4e4e59b5fbe3344cc3496dca6ed
karlosmaor <35969364+karlosmaor@users.noreply.github.com>
2018-10-21 22:29:59 -0500

    asdasdwqeqw

--- controllers/info.js
+++ controllers/info.js
@@ -10,6 +10,24 @@ function getInfos(req, res){
 
     if(err)return res.status(500).send({message:`Error al realizar la petición ${err}`})
     if(infos.length == 0)return res.status(501).send({message:'No hay información registrada'})
+    let informacion = infos[0]
+
+    for(i=0; i<informacion.categorias.length; i++){
+      Empresa.find({category: informacion.categorias[i].name}, (err, empresasLista)=>{
+        if(err)return res.status(500).send({message:`Error al realizar la petición ${err}`})
+
+        informacion.categorias[i].empresas = empresasLista
+      })
+    }
+    res.status(200).send(informacion)
+  })
+}
+
+function getInfos(req, res){
+  Info.find({}).limit(1).sort('-date').exec((err, infos)=>{
+
+    if(err)return res.status(500).send({message:`Error al realizar la petición ${err}`})
+    if(infos.length == 0)return res.status(501).send({message:'No hay información registrada'})
 
     res.status(200).send(infos[0])
   })
@@ -18,9 +36,9 @@ function getInfos(req, res){
 function saveInfo(req,res){
   let infoJson = JSON.parse(req.body.infoJson)
   delete infoJson._id
+  infoJson.date = new Date()
 
   let info = new Info(infoJson)
-  info.date = new Date()
 
   info.save((err, infoStored)=>{
     if(err)return res.status(500).send({message :`Error al guardar la entrega en la base de datos: ${err}`})
