e713ea8bb3a9f0dce8a5d1ebbb2632870f4b71bd
karlosmaor <35969364+karlosmaor@users.noreply.github.com>
2018-10-23 17:41:59 -0500

    dsfesdcd

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ const app =  express()
 const api = require('../routes')
 
 app.use('/info', expressStatic('./public'))
+app.use('/rex', expressStatic('./public/rex'))
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
