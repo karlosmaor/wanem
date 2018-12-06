51ff527adc03d56b0bbbcdacabee734d838d4e8a
karlosmaor <35969364+karlosmaor@users.noreply.github.com>
2018-12-05 22:55:13 -0500

    dadsadwqe

--- server/app.js
+++ server/app.js
@@ -16,6 +16,7 @@ app.use('/maxseguridad', expressStatic('./public/maxseguridad'))
 app.use('/misterdog', expressStatic('./public/misterdog'))
 app.use('/zummos', expressStatic('./public/zummos'))
 app.use('/elnavio', expressStatic('./public/elnavio'))
+app.use('/maloka', expressStatic('./public/maloka'))
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
