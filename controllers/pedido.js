ed8568bd4a2a5006c4737e9bd11ecfa6b817a9ef
karlosmaor <35969364+karlosmaor@users.noreply.github.com>
2019-01-16 16:13:06 -0500

    sadasdasd

--- controllers/pedido.js
+++ controllers/pedido.js
@@ -68,7 +68,7 @@ function savePedido(req,res){
  fetch('http://paramismejoresamigos.top/wanem/enviarmail.php', {
    method: 'POST',
    headers: {'Content-Type':'application/x-www-form-urlencoded'}, // this line is important, if this content-type is not set it wont work
-   body: 'pedido='.concat(req.body.pedidoJson)
+   body: 'pedido=Hay un nuevo Pedido, revisen el sistema'
 }).then(res=>res.json())
   .then(res => console.log(res));
 console.log('pedido='.concat(req.body.pedidoJson))
