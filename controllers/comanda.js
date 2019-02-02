cbfea83de80c26bc3c2311fa5486c431fc174095
karlosmaor <35969364+karlosmaor@users.noreply.github.com>
2019-02-02 12:52:23 -0500

    dsad

--- controllers/comanda.js
+++ controllers/comanda.js
@@ -52,9 +52,9 @@ function getComandasDia(req, res){
     if(err)return res.status(500).send({message:`Error al realizar la petición ${err}`})
     if(comandas.length == 0)return res.status(501).send({message:'No hay pedidos pendientes'})
 
-    const comandasNuevas = comandas.filter(x => x.state == 0)
+    const comandasNuevas = comandas.filter(x => x.state == 1)
     if(comandasNuevas.length>0){
-      Comanda.update({state:0,empresa:req.params.empresaId}, {state:1}, {multi:true}, (err,doc) =>{
+      Comanda.update({state:1,empresa:req.params.empresaId}, {state:1}, {multi:true}, (err,doc) =>{
         if(err)return res.status(500).send({message:`Error al realizar la actualización ${err}`})
           res.status(200).send(comandas)
       })
