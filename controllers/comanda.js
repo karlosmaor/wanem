2b02de87e551dfadb210b9f529680fe5c4d211e2
karlosmaor <35969364+karlosmaor@users.noreply.github.com>
2019-04-09 00:52:54 -0500

    fsdf

--- controllers/comanda.js
+++ controllers/comanda.js
@@ -229,7 +229,7 @@ function searchFecha(req, res){
     empresa: req.body.empresaId,
     state: {'$lte': 3},
     date: {'$gte': start,'$lte': end}
-  }).sort('-date').exec((err, comandas)=>{
+  }).sort('-date').populate('mesero').populate('cajero').exec((err, comandas)=>{
 
     if(err)return res.status(500).send({message:`Error al realizar la petición ${err}`})
     if(comandas.length == 0)return res.status(501).send({message:'No hay pedidos pendientes'})
