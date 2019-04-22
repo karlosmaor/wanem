ea9faa0126a83cd463722e1d8a7db630f2733c30
karlosmaor <35969364+karlosmaor@users.noreply.github.com>
2019-04-22 18:15:47 -0500

    werwerer

--- controllers/pedido.js
+++ controllers/pedido.js
@@ -134,6 +134,7 @@ function updatePedido(req,res){
           date:new Date()
         })
         var totalComanda = 0
+        
         pedidoJson.productos.filter(x => x.empresa == listaEmpresas[i]).forEach(function(product){
           var newProducto = new Producto({
             entregado:false,
